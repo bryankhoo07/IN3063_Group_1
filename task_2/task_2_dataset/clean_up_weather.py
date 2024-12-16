8388b43148d5cd2703c76217e14e2482e3095bda
KPflitsch <Kian.Pflitsch@city.ac.uk>
2024-12-16 19:21:59 +0000

    Renamed directory - updated accordingly in code

--- task_2/task_2_dataset/clean_up_weather.py
+++ task_2/task_2_dataset/clean_up_weather.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 current_dir = os.path.dirname(__file__)
-file_path = os.path.abspath(os.path.join(current_dir, 'task_2_london_weather/london_weather.csv'))
+file_path = os.path.abspath(os.path.join(current_dir, 'task_2_dataset/london_weather.csv'))
 
 if not os.path.exists(file_path):
     print(f"Error: File not found at {file_path}")
@@ -12,6 +12,6 @@ else:
     bad_data = weather_data[weather_data['max_temp'] < weather_data['min_temp']]
     print(f"Number of problematic rows: {len(bad_data)}")
     cleaned_data = weather_data[weather_data['max_temp'] >= weather_data['min_temp']]
-    cleaned_file_path = os.path.join(current_dir, 'task_2_london_weather/london_weather_clean.csv')
+    cleaned_file_path = os.path.join(current_dir, 'task_2_dataset/london_weather_clean.csv')
     cleaned_data.to_csv(cleaned_file_path, index=False)
     print(f"Cleaned dataset saved to {cleaned_file_path}")
